e4f5c1acca5662d889afcb33085c9e63fc6ace7c
lileitongxue <2466868671@qq.com>
2018-09-06 11:25:13 +0800

    change by lilei

--- clean_rs.sh
+++ clean_rs.sh
@@ -13,11 +13,11 @@ echo $deploy |awk -F " " '{for(i=1;i<=NF;i++)print $i}' |while read line
 do
     echo $line
     #echo $line >> rs.txt
-    sudo kubectl get rs -n lilei2 |grep $line |awk '{print $1, $2, $3, $4}' | while read name num1 num2 num3 
+    sudo kubectl get rs -n lilei2 |grep ^$line"-"[0-9] |awk '{print $1, $2, $3, $4}' | while read name num1 num2 num3 
     do
 	if [ "$num1" == "0" -a "$num2" == "0" -a "$num3" == "0" ];then
-	    echo `sudo kubectl  get rs $name  -o yaml  -n lilei2|grep creationTimestamp \
-	    |awk 'NR==1'|sed 's/^[ \t]*//g' |cut -d " " -f 2`,$name >> rs.txt
+	    echo $name,`sudo kubectl  get rs $name  -o yaml  -n lilei2|grep creationTimestamp \
+	    |awk 'NR==1'|sed 's/^[ \t]*//g' |cut -d " " -f 2` >> rs.txt
 	fi
     done
 done
@@ -26,8 +26,8 @@ echo "------------------------------------------------------------"
 echo $deploy |awk -F " " '{for(i=1;i<=NF;i++)print $i}' |while read line
 do
     if [ -f rs.txt ];then
-	wordcount=`cat rs.txt|grep $line |sort -nr|awk 'NR>4'|cut -d "," -f 2|wc -l`
-	rsname=`cat rs.txt|grep $line |sort -nr|awk 'NR>4'|cut -d "," -f 2`
+	wordcount=`cat rs.txt|grep ^$line"-"[0-9] |sort -t "," -nr|awk 'NR>4'|wc -l`
+	rsname=`cat rs.txt|grep ^$line"-"[0-9] |sort -t "," -nr|awk 'NR>4'|cut -d "," -f 1`
 	if [ $wordcount -gt 0 ];then
 	    echo "删除对应deployment:$line下的多余的rs" 
 	    echo "$rsname"
--- clean_rs_new.sh
+++ clean_rs_new.sh
@@ -17,12 +17,11 @@ function delete_rs(){
 	echo $deploy |awk -F " " '{for(i=1;i<=NF;i++)print $i}' |while read line 
 	do
 	    echo $line
-	    #echo $line >> rs.txt
-	    sudo kubectl get rs -n $project |grep $line |awk '{print $1, $2, $3, $4}' | while read name num1 num2 num3 
+	    sudo kubectl get rs -n $project |grep ^$line"-"[0-9] |awk '{print $1, $2, $3, $4}' | while read name num1 num2 num3 
 	    do
 		if [ "$num1" == "0" -a "$num2" == "0" -a "$num3" == "0" ];then
-		    echo `sudo kubectl  get rs $name  -o yaml  -n $project|grep creationTimestamp \
-                    |awk 'NR==1'|sed 's/^[ \t]*//g' |cut -d " " -f 2`,$name >> rs.txt
+		    echo $name,`sudo kubectl  get rs $name  -o yaml  -n $project|grep creationTimestamp \
+                    |awk 'NR==1'|sed 's/^[ \t]*//g' |cut -d " " -f 2`  >> rs.txt
 		fi
 	    done
 	done
@@ -31,8 +30,8 @@ function delete_rs(){
 	echo $deploy |awk -F " " '{for(i=1;i<=NF;i++)print $i}' |while read line
 	do
 	    if [ -f rs.txt ];then
-		wordcount=`cat rs.txt|grep $line |sort -nr|awk 'NR>4'|cut -d "," -f 2|wc -l`
-                rsname=`cat rs.txt|grep $line |sort -nr|awk 'NR>4'|cut -d "," -f 2`
+		wordcount=`cat rs.txt|grep ^$line"-"[0-9] |sort -t "," -nr|awk 'NR>4'|wc -l`
+	        rsname=`cat rs.txt|grep ^$line"-"[0-9] |sort -t "," -nr|awk 'NR>4'|cut -d "," -f 1`
                 if [ $wordcount -gt 0 ];then
 		    echo "删除对应deployment:$line下的多余的rs" 
                     echo "$rsname"
